067b8ae093ba7b24f0e53c8e23201d9e8abfad7c
Administrator <garikgevorgyan1991@gmail.com>
2021-09-11 00:48:34 +0400

    Homework2

--- src/Java/homework/Homework_2/Task2.java
+++ src/Java/homework/Homework_2/Task2.java
@@ -1,23 +1,6 @@
 package Java.homework.Homework_2;
 
 public class Task2 {
-//
-//    public static void main(String[] args) {
-//
-//        char first = 'a';
-//        char second = 'b';
-//        char thired = 'c';
-//
-//        if (first>second) {
-//            if (first>thired){
-//                System.out.println(first);
-//            }
-//        }else if (second>thired) {
-//            System.out.println(second);
-//        }else System.out.println(thired);
-//        }
-//    }
-
 
     public static void main(String[] args) {
         char x = 'b';
